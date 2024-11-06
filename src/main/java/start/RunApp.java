3b042bfcd921bfd278a997be892e53ecb4d8211e
Paula Sanchez Velez <psanchezvelez03@gmail.com>
2024-11-06 12:22:02 +0100

    He creado la clase RunApp, lo que se ejecutará

---
+++ src/main/java/start/RunApp.java
@@ -0,0 +1,22 @@
+package start;
+
+import java.awt.EventQueue;
+
+import view.JFramePrincipal;
+
+public class RunApp {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFramePrincipal frame = new JFramePrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
