64b682363b4cc08d896524116d6fb0f30483fead
Paula Sanchez Velez <psanchezvelez03@gmail.com>
2024-11-06 12:19:45 +0100

    He terminado la interfaz del programa

--- src/main/java/view/JFramePrincipal.java
+++ src/main/java/view/JFramePrincipal.java
@@ -1,123 +1,178 @@
 package view;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JButton;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.PanelDatos;
 
 public class JFramePrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField tfNombre;
-	private JTextField tfFechaNac;
-	private JTextField tfDepart;
-	private JTextField tfSalario;
+	
+	// CREACIÓN PANELES
+	private JPanel panPrincipal, panVer, panDatosVer, panBotones, panAlta, panDatosAlta, panBotonesAlta, panAcerca ;
+	
+	//Creación JLabel	
+	private JLabel lblAcerca;
+	
+	// Creación Botones
+	private JButton btnPrimero, btnRetroceder, btnAvanzar, btnUltimo, btnCancelar, btnAceptar;
+	
+	// Creación menu y opciones
+	private JMenuBar menuPaneles;
+	private JMenuItem mntmVer, mntmAlta, mntmAcerca;
+	
+	
+	
+	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					JFramePrincipal frame = new JFramePrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public JFramePrincipal() {
+				
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 400);
 		setTitle("Practica 2- Carlos y Paula");
 		setLocationRelativeTo(null);
 		setResizable(false);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panPrincipal = new JPanel();
+		panPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(panPrincipal);
+		panPrincipal.setLayout(new BorderLayout(0, 0));
 		
-		JMenuBar menuPaneles = new JMenuBar();
-		contentPane.add(menuPaneles, BorderLayout.NORTH);
+		addComponents();
 		
-		JMenuItem mntmVer = new JMenuItem("Ver Empleados");
+		addListeners();
+		
+			
+		
+	}
+
+	private void addListeners() {
+		
+		 // Acciones de menú
+        mntmAlta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Mostrar "Alta Empleados" y ocultar "Ver Empleados"
+                panPrincipal.remove(panVer);
+                panPrincipal.remove(panAcerca);
+                panPrincipal.add(panAlta, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+
+        mntmVer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Mostrar "Ver Empleados" y ocultar "Alta Empleados"
+                panPrincipal.remove(panAlta);
+                panPrincipal.remove(panAcerca);
+                panPrincipal.add(panVer, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+        
+        mntmAcerca.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Mostrar el panel "Acerca de"
+                panPrincipal.remove(panVer);
+                panPrincipal.remove(panAlta);
+                panPrincipal.add(panAcerca, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+            }
+        });
+		
+	}
+
+	private void addComponents() {
+		
+		
+		menuPaneles = new JMenuBar();
+		panPrincipal.add(menuPaneles, BorderLayout.NORTH);
+		
+		mntmVer = new JMenuItem("Ver Empleados");
 		menuPaneles.add(mntmVer);
 		
-		JMenuItem mntmAlta = new JMenuItem("Alta Empleados");
+		mntmAlta = new JMenuItem("Alta Empleados");
 		menuPaneles.add(mntmAlta);
 		
-		JMenuItem mntmAcerca = new JMenuItem("Acerca de");
+		mntmAcerca = new JMenuItem("Acerca de");
 		menuPaneles.add(mntmAcerca);
 		
-		JPanel panBotones = new JPanel();
-		contentPane.add(panBotones, BorderLayout.SOUTH);
+		// Paneles
+		
+		// Panel VER (Panel Datos + Panel Botones
+		panVer = new JPanel();
+		panVer.setLayout(new BorderLayout(0,0));
+		
+		
+		
+		
+		panDatosVer = new PanelDatos(false);
+		
+		panVer.add(panDatosVer, BorderLayout.CENTER);
+		
+		
+		
+		panBotones = new JPanel();
+		panVer.add(panBotones, BorderLayout.SOUTH);
 		panBotones.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JButton btnPrimero = new JButton("PRIMERO");
+		btnPrimero = new JButton("PRIMERO");
 		panBotones.add(btnPrimero);
 		
-		JButton btnRetroceder = new JButton("RETROCEDER");
+		btnRetroceder = new JButton("RETROCEDER");
 		panBotones.add(btnRetroceder);
 		
-		JButton btnAvanzar = new JButton("AVANZAR");
+		btnAvanzar = new JButton("AVANZAR");
 		panBotones.add(btnAvanzar);
 		
-		JButton btnUltimo = new JButton("ÚLTIMO");
+		btnUltimo = new JButton("ÚLTIMO");
 		panBotones.add(btnUltimo);
 		
-		JPanel panDatos = new JPanel();
-		contentPane.add(panDatos, BorderLayout.CENTER);
-		panDatos.setLayout(new GridLayout(4, 2, 0, 0));
 		
-		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setHorizontalAlignment(SwingConstants.CENTER);
-		panDatos.add(lblNombre);
+		panPrincipal.add(panVer, BorderLayout.CENTER);
 		
-		tfNombre = new JTextField();
-		panDatos.add(tfNombre);
-		tfNombre.setColumns(10);
+		panAlta = new JPanel();
+		panAlta.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblFechaNac = new JLabel("Fecha de Nacimiento");
-		lblFechaNac.setHorizontalAlignment(SwingConstants.CENTER);
-		panDatos.add(lblFechaNac);
+		panDatosAlta = new PanelDatos(true);
 		
-		tfFechaNac = new JTextField();
-		panDatos.add(tfFechaNac);
-		tfFechaNac.setColumns(10);
+		panBotonesAlta = new JPanel();
+		panBotonesAlta.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblDepart = new JLabel("Departamento");
-		lblDepart.setHorizontalAlignment(SwingConstants.CENTER);
-		panDatos.add(lblDepart);
+		btnCancelar = new JButton("CANCELAR");
+		panBotonesAlta.add(btnCancelar);
 		
-		tfDepart = new JTextField();
-		panDatos.add(tfDepart);
-		tfDepart.setColumns(10);
+		btnAceptar = new JButton("ACEPTAR");
+		panBotonesAlta.add(btnAceptar);
 		
-		JLabel lblSalario = new JLabel("Salario");
-		lblSalario.setHorizontalAlignment(SwingConstants.CENTER);
-		panDatos.add(lblSalario);
 		
-		tfSalario = new JTextField();
-		panDatos.add(tfSalario);
-		tfSalario.setColumns(10);
+		panAlta.add(panDatosAlta, BorderLayout.CENTER);
+
+		panAlta.add(panBotonesAlta, BorderLayout.SOUTH);
+		
+		
+		panAcerca = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	    lblAcerca = new JLabel("Proyecto realizado por Carlos Pastor y Paula Sánchez");
+	    panAcerca.add(lblAcerca);  
+	    
+				
+		
 	}
 	
 	
